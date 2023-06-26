7670b9c43696de6087fcf11a6e6477a3753a97dc
viola <1392546252@qq.com>
2023-06-26 10:54:54 +0800

    Layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        <h1>Layout</h1>
-        <!-- 二级路由出口 -->
+        <LayoutNav />  
+        <LayoutHeader /> 
         <router-view></router-view>
-    </div>
+        <LayoutFooter />
 </template>
 
 <script setup>
-
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
 </script>
 
 <style lang="scss" scoped>
